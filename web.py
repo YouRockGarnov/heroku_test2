9b67015cb5eee39684fdb1ae28ba294c5c053d04
Yury Garnov <hamta@yandex.ru>
2018-07-23 22:52:44 +0300

    Update web.py

--- web.py
+++ web.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+confirmation_token = '6d75c23e'
+
+@app.route('/', methods=['POST'])
+def processing():
+      return confirmation_token
+
 @app.route('/')
 def index():
   return 'hello'
