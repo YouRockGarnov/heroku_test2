58ba49301c94a9a2cd3ab8167cd7580b47354f41
Yury Garnov <hamta@yandex.ru>
2018-07-24 17:10:14 +0300

    Update web.py

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 confirmation_token = '6d75c23e'
 returning = 'hello'
+token = 'bddeddc0f08b56addc83768d464cea4246ecb5e1b2c9b5df894900aeb84dcec396cc77eae7a5b6062bd6f'
 
 @app.route('/', methods=['POST'])
 def processing():
